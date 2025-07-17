69a7d266ce8661ecab4ec49a06835383206f1001
Rahul Malhotra <rahulmalhotra236@gmail.com>
2025-07-17 13:51:02 +0530

    added the scheme

---
+++ db.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+const ObjectId = mongoose.Types.ObjectId
+
+console.log("db connected")
+mongoose.connect(
+  "mongodb+srv://rahulmalhotra236:Ck986y5LCQSGjTps@cluster0.jlt65zx.mongodb.net/coursera-app"
+)
+
+const userSchema = new Schema({
+  email: { type: String, unique: true },
+  password: String,
+  firstName: String,
+  lastName: String,
+})
+const adminSchema = new Schema({
+  email: { type: String, unique: true },
+  password: String,
+  firstName: String,
+  lastName: String,
+})
+
+const courseSchema = new Schema({
+  title: String,
+  price: Number,
+  description: String,
+  ImageUrl: String,
+  creatorId: ObjectId,
+})
+const purchaseSchema = new Schema({
+  user: ObjectId,
+  corse: ObjectId,
+})
+
+const userModel = mongoose.model("user", userSchema)
+const courseModel = mongoose.model("course", courseSchema)
+const adminModel = mongoose.model("admin", adminSchema)
+const purchaseModel = mongoose.model("purchase", purchaseSchema)
+
+module.exports = {
+  userModel,
+  courseModel,
+  adminModel,
+  purchaseModel,
+}
