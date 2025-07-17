eb90777158bc7ce5dde8095d6e717a3d2bb84f07
Rahul Malhotra <rahulmalhotra236@gmail.com>
2025-07-17 12:43:29 +0530

    added few packages

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express")
+
+const app = express()
+
+app.post("/user/sigup", (req, res) => {})
+app.post("/user/sigin", (req, res) => {})
+app.post("/course/purchase", (req, res) => {})
+app.get("/courses", (req, res) => {})
+app.get("/user/purchases", (req, res) => {})
