707046cb294ec58812da1d135fa9498e5434d651
Rahul Malhotra <rahulmalhotra236@gmail.com>
2025-07-17 13:17:25 +0530

    added admin route

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express")
+const { adminRouter } = require("./routes/admin")
 const { courseRouter } = require("./routes/course")
 const { userRouter } = require("./routes/user")
 
 const app = express()
 
 app.use("/api/v1/user", userRouter)
+app.use("/api/v1/admin", adminRouter)
 app.use("/api/v1/course", courseRouter)
 
 app.listen(3000, () => {
